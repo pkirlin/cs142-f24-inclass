23bf56da70bcb2fc2ad2b26ecfeb408149dac2b0
Phil Kirlin <kirlinp@rhodes.edu>
2024-09-09 09:56:26 -0500

    functions

--- src/functions/FunctionPractice.java
+++ src/functions/FunctionPractice.java
@@ -1,4 +1,13 @@
 package functions;
 
 public class FunctionPractice {
+    public static void main(String[] args) {
+        double answer = distance(1, 2, 3, -4);
+        System.out.println(Math.round(answer));
+    }
+
+    public static double distance(int x1, int y1, int x2, int y2) {
+        return Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2));
+    }
+
 }

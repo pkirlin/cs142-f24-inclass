fa9b99edda4c39290445248b1557122f484ba535
Phil Kirlin <kirlinp@rhodes.edu>
2024-09-13 10:54:55 -0500

    oop1

--- src/oop1/SimpleCanvasDemo1.java
+++ src/oop1/SimpleCanvasDemo1.java
@@ -11,13 +11,13 @@ public class SimpleCanvasDemo1 {
         // Instantiate a new SimpleCanvas object by calling a constructor.
         // This creates a new instance of the SimpleCanvas class, and assigns
         // that instance to the canvas variable.
-        canvas = new SimpleCanvas(400, 200, "My Canvas");
+        canvas = new SimpleCanvas(400, 500, "My Canvas");
 
         // Note that we could have done both lines together:
         // SimpleCanvas canvas = new SimpleCanvas(400, 200, "My Canvas");
 
         // Invoke some instance methods that operate on the canvas.
-        canvas.setPenColor(Color.BLUE);
+        canvas.setPenColor(Color.GREEN);
         canvas.drawFilledRectangle(100, 50, 50, 100);
         canvas.setPenColor(Color.RED);
         canvas.setLineThickness(5);

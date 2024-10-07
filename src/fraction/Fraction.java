e02707e0a8f538e33a7a6ef7f3b5beda358c06a3
Phil Kirlin <kirlinp@rhodes.edu>
2024-10-07 09:35:31 -0500

    fraction class

--- src/fraction/Fraction.java
+++ src/fraction/Fraction.java
@@ -1,5 +1,29 @@
 package fraction;
 
 public class Fraction {
+    private int numer, denom;
 
+    public Fraction(int newNumer, int newDenom) {
+        numer = newNumer;
+        denom = newDenom;
+        if (denom < 0) {
+            numer = -numer;
+            denom = -denom;
+        }
+        int gcd = ExtraMath.gcd(numer, denom);
+        numer = numer / gcd;
+        denom = denom / gcd;
+    }
+
+    public String toString() {
+        return numer + "/" + denom;
+    }
+
+    public Fraction multiply(Fraction otherFraction) {
+        int newNumer = numer * otherFraction.numer;
+        int newDenom = denom * otherFraction.denom;
+        System.out.println("before reducing " + newNumer + " " + newDenom);
+        Fraction product = new Fraction(newNumer, newDenom);
+        return product;
+    }
 }
--- src/fraction/FractionDemo.java
+++ src/fraction/FractionDemo.java
@@ -1,5 +1,11 @@
 package fraction;
 
 public class FractionDemo {
-
+    public static void main(String[] args) {
+        Fraction f = new Fraction(2, 3);
+        Fraction f2 = new Fraction(6, 8);
+        System.out.println(f);
+        System.out.println(f2);
+        System.out.println(f.multiply(f2));
+    }
 }

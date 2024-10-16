84b928838b57a896bc61af2570089ad5a57ccdf5
Phil Kirlin <kirlinp@rhodes.edu>
2024-10-16 08:25:06 -0500

    prebreak

--- src/prebreak/Exercises.java
+++ src/prebreak/Exercises.java
@@ -1,15 +1,32 @@
 package prebreak;
 
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Exercises {
 
     public static void main(String[] args) {
-
+        ArrayList<Integer> lst = new ArrayList<Integer>();
+        lst.add(1);
+        lst.add(2);
+        lst.add(3);
+        lst.add(4);
+        lst.add(5);
+        System.out.println(lst);
+        reverseArrayList(lst);
+        System.out.println(lst);
+        readWords();
     }
 
     public static void reverseArrayList(ArrayList<Integer> list) {
-
+        int R = list.size() - 1;
+        for (int L = 0; L < list.size()/2; L++) {
+            int temp = list.get(L);
+            list.set(L, list.get(R));    // list[L] = list[R]
+            list.set(R, temp);
+            R--;
+        }
     }
 
     public static String reverseString(String str) {
@@ -17,7 +34,18 @@ public class Exercises {
     }
 
     public static ArrayList<String> readWords() {
-        return null;
+        InputStream is = Exercises.class.getResourceAsStream("words.txt");
+        if (is == null) {
+            System.out.println("Wrong file name!");
+        }
+        Scanner scanner = new Scanner(is);
+        ArrayList<String> list = new ArrayList<String>();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            System.out.println(line);
+            list.add(line);
+        }
+        return list;
     }
 
     public static int linearSearch(ArrayList<String> list, String lookingFor) {
